ea2d3cc5ed6b7818c2e600bd217f5064dcb7627c
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-05-13 12:23:23 -0600

    Set up basic models and auditing fields

---
+++ src/main/java/com/example/crudsteels/models/Applications.java
@@ -0,0 +1,10 @@
+package com.example.crudsteels.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "application")
+public class Applications {
+}

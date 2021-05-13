f201edc377d4fe60df3b0aa8bc2074efdc857834
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-05-13 10:56:57 -0600

    Initial Commit

---
+++ src/main/java/com/example/crudsteels/CrudSteelsApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudsteels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSteelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSteelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudsteels/CrudSteelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudsteels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSteelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

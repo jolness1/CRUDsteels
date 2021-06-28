8dd845911dccdca42498ec112e47e961b85d98d9
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-06-28 12:32:03 -0600

    Added Roles, Joined User/Roles

--- src/main/java/com/example/crudsteels/models/User.java
+++ src/main/java/com/example/crudsteels/models/User.java
@@ -1,10 +1,18 @@
 package com.example.crudsteels.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.*;
 
 import javax.validation.constraints.Email;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
@@ -25,6 +33,14 @@ public class User extends Auditable
     @Email
     private String useremail;
 
+    private String comments;
+
+    @OneToMany(mappedBy = "user",
+    cascade = CascadeType.ALL,
+    orphanRemoval = true)
+    @JsonIgnoreProperties(value = "user",allowSetters = true)
+    private Set<UserRoles> roles = new HashSet<>();
+
     public User()
     {
     }
@@ -32,9 +48,10 @@ public class User extends Auditable
     public User(long userid, String username, String password, String useremail)
     {
         this.userid = userid;
-        this.username = username;
-        this.password = password;
+        setUsername(username);
+        setPassword(password);
         this.useremail = useremail;
+        this.comments = comments;
     }
 
     public long getUserid()
@@ -64,6 +81,12 @@ public class User extends Auditable
 
     public void setPassword(String password)
     {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public void setPasswordNoEncrypt(String password)
+    {
         this.password = password;
     }
 
@@ -75,6 +98,41 @@ public class User extends Auditable
     public void setUseremail(String useremail)
     {
         this.useremail = useremail;
+    }
+
+    public String getComments()
+    {
+        return comments;
+    }
+
+    public void setComments(String comments)
+    {
+        this.comments = comments;
+    }
+
+    public Set<UserRoles> getRoles()
+    {
+        return roles;
+    }
+
+    public void setRoles(Set<UserRoles> roles)
+    {
+        this.roles = roles;
+    }
+
+    @JsonIgnore
+    public List<SimpleGrantedAuthority> getAuthority()
+    {
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+
+        for (UserRoles r : this.roles)
+        {
+            String myRole = "ROLE" + r.getRole()
+                    .getName()
+                    .toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority(myRole));
+        }
+        return rtnList;
     }
 }
 
